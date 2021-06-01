b0451b904e8371b3d234e935bcf725a5030dc18f
son <tnh3113@naver.com>
2021-06-01 20:46:17 +0900

    21.06.01's task

---
+++ .algo/src/L_Coder/571.py
@@ -0,0 +1,19 @@
+# C code
+
+# #define _CRT_SECURE_NO_WARNINGS
+# #include <stdio.h>
+
+# void printStr(int num);
+
+# int main() {
+# 	int num;
+# 	scanf("%d", &num);
+# 	printStr(num);
+
+# 	return 0;
+# }
+
+# void printStr(int num) {
+# 	for (int i = 0; i < num; i++)
+# 		printf("~!@#$^&*()_+|\n");
+# }
